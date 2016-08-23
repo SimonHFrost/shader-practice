ee5fc5b1f714d9e76c986a256624f57c1bf98f2f
simonhfrost <simonhfrost@gmail.com>
2016-08-23 22:08:45 +0200

    Tidy app.js even more

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@ var createLight = require('./object-creator.js').createLight;
 var createDirectionalLight = require('./object-creator.js').createDirectionalLight;
 
 var scene = initialize();
-var cube = createCube();
-
-scene.add(cube);
 scene.add(createLight());
 scene.add(createDirectionalLight());
+
+var cube = createCube();
+scene.add(cube);
 
 function rotateCube() {
   cube.rotation.y -= 0.01;
