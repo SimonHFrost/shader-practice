dbc7c8378aaff5bf5a6fff28d30d2099a9d68854
simonhfrost <simonhfrost@gmail.com>
2016-08-23 22:07:45 +0200

    Clean up app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-/* global THREE */
-
 var initialize = require('./initializer.js').initialize;
 var createCube = require('./object-creator.js').createCube;
 var createLight = require('./object-creator.js').createLight;
@@ -9,12 +7,11 @@ var scene = initialize();
 var cube = createCube();
 
 scene.add(cube);
+scene.add(createLight());
+scene.add(createDirectionalLight());
 
 function rotateCube() {
   cube.rotation.y -= 0.01;
 }
 
 window.setInterval(rotateCube, 20);
-
-scene.add(createLight());
-scene.add(createDirectionalLight());
