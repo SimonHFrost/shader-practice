000e01618a3325cf7e39d28de09f7429fe6610c8
simonhfrost <simonhfrost@gmail.com>
2016-08-23 22:04:20 +0200

    Don't import model

--- js/app.js
+++ js/app.js
@@ -17,28 +17,6 @@ function rotatePlanet() {
   cube.rotation.y -= 0.01;
 }
 
-loader.load(modelPath, function (geometry) {
-  var material = new THREE.MeshLambertMaterial({
-    color: '#ed8989',
-    shading: THREE.FlatShading
-  });
-
-  spaceShip = new THREE.Mesh(
-    geometry,
-    material
-  );
-
-  spaceShip.scale.x = 0.1;
-  spaceShip.scale.y = 0.1;
-  spaceShip.scale.z = 0.1;
-
-  spaceShip.position.z = 1;
-
-  cube = createCube();
-  cube.add(spaceShip);
-  scene.add(cube);
-});
-
 window.setInterval(rotatePlanet, 20);
 
 scene.add(createLight());
